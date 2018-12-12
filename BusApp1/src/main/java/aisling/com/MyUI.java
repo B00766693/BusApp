2d4fce7a033bdbf115614f825aef82f699939221
“B00766693” <obroin-a@ulster.ac.uk>
2018-12-12 21:15:27 +0000

    layout changes

--- BusApp1/src/main/java/aisling/com/MyUI.java
+++ BusApp1/src/main/java/aisling/com/MyUI.java
@@ -1,37 +1,38 @@
 package aisling.com;
 
-import javax.servlet.annotation.WebServlet;
-
-import java.sql.*;
-import java.util.*;
-import java.util.List;
-import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
-import com.vaadin.ui.Grid;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Grid.SelectionMode;
-import com.vaadin.ui.*;
-import com.vaadin.sass.internal.parser.Variable;
+import javax.servlet.annotation.WebServlet;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.Grid;
+import com.vaadin.ui.Grid.SelectionMode;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Slider;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
- * (or tab) or some part of an HTML page where a Vaadin application is embedded.
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of an HTML page where a Vaadin application is
+ * embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
- * overridden to add component to the user interface and initialize non-component functionality.
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
  */
 @Theme("mytheme")
 public class MyUI extends UI {
@@ -39,14 +40,10 @@ public class MyUI extends UI {
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         Connection connection = null;
-        String connectionString = "jdbc:sqlserver://tuesserver.database.windows.net:1433;" + 
-        "database=BusDatabase;" + 
-        "user=aislingstudent@tuesserver;" + 
-        "password=Server123;" + 
-        "encrypt=true;" + 
-        "trustServerCertificate=false;" + 
-        "hostNameInCertificate=*.database.windows.net;" +
-        "loginTimeout=30;";
+        String connectionString = "jdbc:sqlserver://tuesserver.database.windows.net:1433;" + "database=BusDatabase;"
+                + "user=aislingstudent@tuesserver;" + "password=Server123;" + "encrypt=true;"
+                + "trustServerCertificate=false;" + "hostNameInCertificate=*.database.windows.net;"
+                + "loginTimeout=30;";
         //to do a connection on the app
 
         final VerticalLayout layout = new VerticalLayout();
@@ -152,7 +149,7 @@ public class MyUI extends UI {
 
         hLayout.addComponents(groupName,s, accesibilityRequired);
 
-        layout.addComponent(myGrid);
+        layout.addComponents(hLayout, button, infoLabel ,myGrid);
 
         }//try
 
